f9e2d3c16a1c76b5f8e3643299589a11857baf52
sonic011gamer <58566108+sonic011gamer@users.noreply.github.com>
2025-01-25 19:24:53 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Device
-export FOX_BRANCH="fox_9.0"
-export DT_LINK="https://gitlab.com/OrangeFox/device/beryllium.git -b fox_9.0"
+export FOX_BRANCH="fox_8.1"
+export DT_LINK="https://github.com/sonic011gamer/ofox_cityman -b fox_8.1"
 
-export DEVICE="beryllium"
-export OEM="xiaomi"
+export DEVICE="cityman"
+export OEM="microsoft"
 
 # Build Target
 ## "recoveryimage" - for A-Only Devices without using Vendor Boot
@@ -22,7 +22,7 @@ DEPS=(
 )
 
 # Extra Command
-export EXTRA_CMD="export OF_MAINTAINER=Sushrut1101"
+export EXTRA_CMD="export OF_MAINTAINER=sonic011gamer"
 
 # Magisk
 ## Use the Latest Release of Magisk for the OrangeFox addon
